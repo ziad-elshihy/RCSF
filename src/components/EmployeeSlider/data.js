ae428ab08b8c6b5f95385d42fa136a444fc9a2f3
ziad-elshihy <elshihyziad@gmail.com>
2023-09-16 21:03:47 +0300

    .

--- src/components/EmployeeSlider/data.js
+++ src/components/EmployeeSlider/data.js
@@ -1,71 +1,71 @@
 const people = [
    {
       id: 1,
-      image: '../../../public/photo_2023-09-16_19-58-02.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 2,
-      image: '../../../public/photo_2023-07-03_03-10-56.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 3,
-      image: '../../../public/photo_2023-09-16_19-58-02.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 4,
-      image: '../../../public/photo_2023-07-03_03-10-56.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    }, {
       id: 5,
-      image: '../../../public/photo_2023-09-16_19-58-02.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 6,
-      image: '../../../public/photo_2023-07-03_03-10-56.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 7,
-      image: '../../../public/photo_2023-09-16_19-58-02.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 8,
-      image: '../../../public/photo_2023-07-03_03-10-56.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    }, {
       id: 9,
-      image: '../../../public/photo_2023-09-16_19-58-02.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 10,
-      image: '../../../public/photo_2023-07-03_03-10-56.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 11,
-      image: '../../../public/photo_2023-09-16_19-58-02.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 12,
-      image: '../../../public/photo_2023-07-03_03-10-56.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
