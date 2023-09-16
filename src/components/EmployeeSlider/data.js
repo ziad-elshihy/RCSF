44249074c9776a7e48bde8633cca0b0a38b11a73
ziad-elshihy <elshihyziad@gmail.com>
2023-09-16 21:10:45 +0300

    ....

--- src/components/EmployeeSlider/data.js
+++ src/components/EmployeeSlider/data.js
@@ -1,65 +1,65 @@
 const people = [
    {
       id: 1,
-      image: '../../../public/1.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 2,
-      image: '../../../public/2.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 3,
-      image: '../../../public/1.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 4,
-      image: '../../../public/2.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    }, {
       id: 5,
-      image: '../../../public/1.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 6,
-      image: '../../../public/2.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 7,
-      image: '../../../public/1.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 8,
-      image: '../../../public/2.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    }, {
       id: 9,
-      image: '../../../public/1.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 10,
-      image: '../../../public/2.jpg',
+      image: '../../../public/1.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
    {
       id: 11,
-      image: '../../../public/1.jpg',
+      image: '../../../public/2.jpg',
       name: 'Ziad Elshihy',
       title: 'the boss'
    },
