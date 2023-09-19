f2f57e6ccb7db99d37c059b47db38ad0af54bf6a
ziad-elshihy <elshihyziad@gmail.com>
2023-09-19 21:39:30 +0300

    ......

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,25 @@ import Home from './Pages/Home';
 import Regulations from './Pages/regulations/Reg';
 import MissionVision from './Pages/mission&vision/MissionVision';
 import Footer from './components/footer/Footer';
+import { Context } from './context/Context.jsx';
 
 const App = () => {
 
    return (
       <>
-         <Navbar />
-         <Routes >
-            <Route path='/' element={<Home />} />
-            <Route path='/director' element={<ExecutiveDirector />} />
-            <Route path='/StrategicGoals' element={<StrategicGoals />} />
-            <Route path='/Patrols' element={<Patrols />} />
-            <Route path='/Regulations' element={<Regulations />} />
-            <Route path='/mission&vision' element={<MissionVision />} />
-            {/* <Route path='*' element={<PageNotFound />} /> */}
-         </Routes>
-         <Footer />
+         <Context >
+            <Navbar />
+            <Routes >
+               <Route path='/' element={<Home />} />
+               <Route path='/director' element={<ExecutiveDirector />} />
+               <Route path='/StrategicGoals' element={<StrategicGoals />} />
+               <Route path='/Patrols' element={<Patrols />} />
+               <Route path='/Regulations' element={<Regulations />} />
+               <Route path='/mission&vision' element={<MissionVision />} />
+               {/* <Route path='*' element={<PageNotFound />} /> */}
+            </Routes>
+            <Footer />
+         </Context >
       </>
 
    )
