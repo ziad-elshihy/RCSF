9179c6d8decf3c865040496c8313d7ce58c43d32
ziad-elshihy <elshihyziad@gmail.com>
2023-09-28 01:33:01 +0300

    new background for homepage

--- src/components/mainsection/HomePage.jsx
+++ src/components/mainsection/HomePage.jsx
@@ -1,12 +1,21 @@
 import image from '../../assets/images/rscf (Large).png'
+import { useContext, useState } from 'react'
+import { dataContext } from '../../context/Context';
+
+
 import "./home.css"
 
-
 function HomePage() {
+   const { lang } = useContext(dataContext)
    return (
       <div className='slide__s'>
          <div className='image container'>
             <img className="big-logo" src={image} alt="Logo" />
+            {
+               lang
+                  ? <h2>المركز الإقليمي للتمويل المستدام</h2>
+                  : <h2 className='text-45'>Regional Center For Sustainable Finance</h2>
+            }
          </div>
       </div>
    )
